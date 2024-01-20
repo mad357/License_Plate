54d3b33150e1bcc5f5e5a3dfaa92b7dbe2976f77
mad357 <pawel_czapnik@wp.pl>
2024-01-20 21:02:36 +0100

    speed up algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(licensePlate(0));
         System.out.println(licensePlate(-5));
+        System.out.println(licensePlate(0));
         System.out.println(licensePlate(111));
         System.out.println(licensePlate(123456));
-        System.out.println(licensePlate(1204567));
+        System.out.println(licensePlate(1000000));
+        System.out.println(licensePlate(1000001));
+        System.out.println(licensePlate(1104567));
         System.out.println(licensePlate(3500000));
         System.out.println(licensePlate(3599999));
         System.out.println(licensePlate(3600000));
+        System.out.println(licensePlate(3610000));
+        System.out.println(licensePlate(3850000));
         System.out.println(licensePlate(3860000));
-        System.out.println(licensePlate(4119999));
-        System.out.println(licensePlate(4120000));
+        System.out.println(licensePlate(4110023));
+        System.out.println(licensePlate(4120023));
         System.out.println(licensePlate(10350000));
         System.out.println(licensePlate(10360000));
-        System.out.println(licensePlate(190360000));
-        System.out.println(licensePlate(192447359));
+        System.out.println(licensePlate(27935000));
+        System.out.println(licensePlate(27936000));
+        System.out.println(licensePlate(73633500));
+        System.out.println(licensePlate(73633600));
+        System.out.println(licensePlate(192447350));
         System.out.println(licensePlate(192447360));
-        System.out.println(licensePlate(392447360));
         System.out.println(licensePlate(501363135));
         System.out.println(licensePlate(501363136));
         System.out.println(licensePlate(999999999));
@@ -30,59 +36,76 @@ public class Main {
     }
 
     public static String toAlphabetic(int i) {
-        int quot = i/26;
-        int rem = i%26;
-        char letter = (char)((int)'A' + rem);
-        if( quot == 0 ) {
-            return ""+letter;
+        if (i < 0) {
+            return "-" + toAlphabetic(-i - 1);
+        }
+
+        int quot = i / letters().length();
+        int rem = i % letters().length();
+        char letter = (char) ((int) 'A' + rem);
+
+        if (quot == 0) {
+            return "" + letter;
         } else {
             return toAlphabetic(quot) + letter;
         }
     }
+    public static int removeLeadingDigits(int number, int digitsToRemove) {
+        if (number == 0) {
+            return 0;
+        }
+        if (getNumberOfDigits(number) == digitsToRemove) {
+            return number;
+        }
 
-    private static String licensePlate(long index) {
+        digitsToRemove = Math.min(digitsToRemove, getNumberOfDigits(number));
+        int divisor = (int) Math.pow(numbers().length(), digitsToRemove);
+
+        return number % divisor;
+    }
+
+    public static int getNumberOfDigits(int number) {
+        return (int) Math.log10(number) + 1;
+    }
+    private static String licensePlate(int index) {
         if (index < 0) {
             return "Out of range";
         }
         String plate = "";
         int lettersNumber = 0;
-        int lettersDecimalValue = 0;
-
-        for (int i = 6; i > 0; i--) {
-            if (Math.pow(numbers().length(), i) <= index) {
-                lettersDecimalValue = 0;
-                index -= Math.pow(numbers().length(), i);
+        int maxAvailableValue = 0;
+        for (int i = 1; i <= 7; i++) {
+            maxAvailableValue += (Math.pow(numbers().length(), 7 - i) * Math.pow(letters().length(), i - 1));
+            if (i < 7 && maxAvailableValue - 1 < index) {
                 lettersNumber++;
-                for (int j = 0; j < Math.pow(letters().length(), lettersNumber) - 1; j++) {
-                    if (Math.pow(numbers().length(), i-1) <= index) {
-                        index -= Math.pow(numbers().length(), i-1);
-                        lettersDecimalValue++;
-                    }
-                    else {
-                        break;
-                    }
-                }
             }
             else {
                 break;
             }
-        }
-        if (lettersNumber > 0) {
-            plate = toAlphabetic(lettersDecimalValue);
-        }
 
+        }
+        if (maxAvailableValue - 1 < index) {
+            return "Out of range";
+        }
+        for (int i = lettersNumber; i > 0; i--) {
+            int temp = (int) (Math.pow(numbers().length(), 7 - i) * Math.pow(letters().length(), i - 1));
+            index -= temp;
+        }
+        int decimalIndex = removeLeadingDigits(index, 6 - lettersNumber);
+        index -= decimalIndex;
+        index /= Math.pow(numbers().length(), 6 - lettersNumber);
+        if (lettersNumber > 0) {
+            plate = toAlphabetic(index);
+        }
         while (plate.length() < lettersNumber) {
             plate = "A" + plate;
         }
-        if (index > 0) {
-            plate = index + plate;
-        }
-        if (plate.length() > 6) {
-            return "Out of range";
+        if (decimalIndex != 0) {
+            plate = decimalIndex + plate;
         }
         while (plate.length() < 6) {
             plate = "0" + plate;
         }
-            return plate;
-        }
+        return plate;
+    }
 }
